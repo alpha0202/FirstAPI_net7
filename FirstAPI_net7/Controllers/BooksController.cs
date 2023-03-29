182b35fd3576f6fa4bb2a1ebffa5fea4e65f496b
edwin martinez <edwmartinezg@outlook.com>
2023-03-28 22:15:00 -0500

    Terminación de los diferentes endpoint.

--- FirstAPI_net7/Controllers/BooksController.cs
+++ FirstAPI_net7/Controllers/BooksController.cs
@@ -1,6 +1,7 @@
 ﻿using FirstAPI_net7.Data;
 using FirstAPI_net7.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FirstAPI_net7.Controllers
 {
@@ -22,7 +23,7 @@ namespace FirstAPI_net7.Controllers
         public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
         {
 
-            return;
+            return await _context.Books.ToListAsync();
         }
 
 
@@ -31,7 +32,12 @@ namespace FirstAPI_net7.Controllers
         public async Task<ActionResult<Book>> GetBookById(int id)
         {
                 
-
+            var book = await _context.Books.FindAsync(id);  
+            if (book == null) 
+               return NotFound();
+                
+            
+            return Ok(book);
         }
 
 
@@ -41,6 +47,13 @@ namespace FirstAPI_net7.Controllers
         public async Task<ActionResult<Book>> PostBook(Book book)
         {
 
+            _context.Books.Add(book);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBookById", new { id = book.Id }, book);
+
+
         }
 
 
@@ -48,14 +61,42 @@ namespace FirstAPI_net7.Controllers
         [HttpPut("{id}")]
         public async Task<IActionResult> PutBook(int id, Book book)
         {
+            if (id != book.Id)
+            {
+                return BadRequest();
+
+
+            }
+             
+            var bookInDb = await _context.Books.FindAsync(id);
+
+            if (bookInDb == null)
+            {
+                return NotFound();
+
+            }
+
+            _context.Entry(book).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+            return NoContent();
 
         }
-
 
         //delete: api/books/2
         [HttpDelete("{id]")]
         public async Task<ActionResult<Book>> DeleteBook(int id)
         {
+
+            var bookToDel = await _context.Books.FindAsync(id);
+
+            if (bookToDel == null)
+                return NotFound();
+
+            _context.Books.Remove(bookToDel);
+            await _context.SaveChangesAsync();
+
+            return Ok(bookToDel);
 
         }
 
